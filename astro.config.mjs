1dfe95d3921a6397fca04da15b79786dba601545
maazis <maazism@outlook.com>
2025-04-30 11:20:42 +0200

    v0.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import { defineConfig, fontProviders } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.moazz.co",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
